d1d26a9d82b1232b6fd27f53f2f94b81874b1403
saveliyb <saveliyb@gmail.com>
2020-06-11 21:23:51 -0700

    fixed bad if statement in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 mkdir -p build
+
 cd build
-if ! hash vcpkg 2>/dev/null 
+
+if [ ! -d "vcokg" ]
     then git clone https://github.com/Microsoft/vcpkg.git 
 fi
 
@@ -14,11 +16,10 @@ then
 fi
 
 VCPKG_COMMAND=vcpkg install --recurse
-VCPKG_DEFAULT_TRIPLET=x64-linux
 # Add your library ports here. 
 $VCPKG_COMMAND boost-dynamic-bitset boost-integer boost-functional boost-move magic-enum
 cd ..
 
-cmake -Ax64 -DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake ..
+cmake -DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake ..
 cmake --build . --target ALL_BUILD --config Release
 cd ..
